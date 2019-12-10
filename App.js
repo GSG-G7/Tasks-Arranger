323bb3115f506c39946e78f8d3e004c7f3191155
Asmaathabet <asmathabet2009@hotmail.com>
2019-12-10 14:47:29 +0200

    add firebase inital configuration

--- App.js
+++ App.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "<MY_API_KEY>",
+  authDomain: "<MY_AUTH_Domain>",
+  databaseURL: "<YOUR_DATABASE_URL>",
+  storageBucket: "<YOUR_STORAGE_BUCKET>"
+}
+
+firebase.initializeApp(firebaseConfig);
+
 export default function App() {
   return (
     <View style={styles.container}>
