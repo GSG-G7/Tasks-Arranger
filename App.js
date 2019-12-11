2789acd8e2441135d1b4cecccf0b9b4be4d2fa1f
Asmaathabet <asmathabet2009@hotmail.com>
2019-12-11 10:17:04 +0200

    create pages folder & seprate login in login page & change app to function

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
 import * as firebase from 'firebase';
+
+import Login from './pages/Login';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBsVDmkwmJSwgfcRWbc8YBpDhBe9fCf5F8",
@@ -13,83 +14,19 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-import { Container, Content, Header, Form, Input, Item, Button, Label } from 'native-base';
 
-export default class App extends React.Component {
-  render(){
-    return (
-      <Container style={styles.container}>
-        <form>
-          <Item  style={styles.item}>
-            <Label style={styles.label}>Email</Label>
-            <Input
-                autoCorrect={false}
-                autoCapitalize="none"
-                style={styles.input}
-            ></Input>
-          </Item>
-  
-          <Item  style={styles. item} >
-            <Label style={styles.label}>Password</Label>
-            <Input
-                secureTextEntry={true}
-                autoCorrect={false}
-                autoCapitalize="none"
-                style={styles.input}
-            ></Input>
-          </Item>
-  
-          <Button
-              full 
-              rounded
-              success
-              style={styles.login}
-              >
-            <Text style={styles.buttonTxt}> Login </Text>
-          </Button>
-  
-          <Button
-              full 
-              rounded
-              style={styles.signup}
-              >
-            <Text style={styles.buttonTxt}> Sign Up </Text>
-          </Button>
-        </form>
-      </Container>
-    );
-  }
+const App = () => { 
+  return(
+    <View style={styles.conatiner}>
+      <Login />    
+    </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-    marginLeft:10,
-    marginRight: 10
-  },
-  item: {
-    marginTop: 5,
-    marginBottom:20,
-    paddingBottom:10
-  },
-  label:{
-    marginBottom:10
-  },
-  input: {
-   fontSize: 16,
-  },
-  login:{ 
-    marginTop: 10,
-    marginBottom:10
-  },
-  signup:{
-   backgroundColor :'#3f90b5'
-  },
-  buttonTxt: {
-    color: "#fff",
-    fontSize:20,
-    fontWeight: 'bold',
+  conatiner:{
+    flex:1,
   }
-});
+})
+
+export default App ;
---
+++ pages/Login.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import { Container, Content, Header, Form, Input, Item, Button, Label } from 'native-base';
+
+class Login extends React.Component {
+  state= [
+    {
+      email: '',
+      password: ''
+    }
+  ]
+
+  render(){
+    return (
+      <Container style={styles.container}>
+        <form>
+          <Item  style={styles.item}>
+            <Label style={styles.label}>Email</Label>
+            <Input
+                autoCorrect={false}
+                autoCapitalize="none"
+                style={styles.input}
+            ></Input>
+          </Item>
+  
+          <Item  style={styles. item} >
+            <Label style={styles.label}>Password</Label>
+            <Input
+                secureTextEntry={true}
+                autoCorrect={false}
+                autoCapitalize="none"
+                style={styles.input}
+            ></Input>
+          </Item>
+  
+          <Button
+              full 
+              rounded
+              success
+              style={styles.login}
+              >
+            <Text style={styles.buttonTxt}> Login </Text>
+          </Button>
+  
+          <Button
+              full 
+              rounded
+              style={styles.signup}
+              >
+            <Text style={styles.buttonTxt}> Sign Up </Text>
+          </Button>
+        </form>
+      </Container>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    marginLeft:10,
+    marginRight: 10
+  },
+  item: {
+    marginTop: 5,
+    marginBottom:20,
+    paddingBottom:10
+  },
+  label:{
+    marginBottom:10
+  },
+  input: {
+   fontSize: 16,
+  },
+  login:{ 
+    marginTop: 10,
+    marginBottom:10
+  },
+  signup:{
+   backgroundColor :'#3f90b5'
+  },
+  buttonTxt: {
+    color: "#fff",
+    fontSize:20,
+    fontWeight: 'bold',
+  }
+});
+
+export default Login;
